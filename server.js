321f2f437d8f99b824910cf20d46ef5175bbc2d5
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-09-18 11:14:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-1', function (req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 app.get('/article-2', function (req,res){
-   res.send('Article 2 requested and will be served here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
 });
 app.get('/article-3', function (req,res){
-   res.send('Article 3 requested and will be served here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-3.html')); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
