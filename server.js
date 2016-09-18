b435c144551b6d1bbd2d2e865d3abf29cd9e6115
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-09-18 13:12:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ return htmlTemplate
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articleName', function (req,res){
+app.get('/:articleName', function (req,res){
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
