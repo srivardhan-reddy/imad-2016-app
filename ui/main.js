89b9d8b9a0cc160869242321340407a50a4cc154
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-09-25 19:09:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,3 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'hey man';
 
 
-var img = document.getElementById('madi');
-img.onclick =  functio (){
-    img.style.marginleft = '200px';
-}
