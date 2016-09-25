4821c789722c7123f30a9a70afab8813d730687c
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-09-25 19:29:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'hey man';
 
-
+var img = document.getElementById('madi');
+img.onclick = function (){
+    img.style.marginleft='100px';
+};
