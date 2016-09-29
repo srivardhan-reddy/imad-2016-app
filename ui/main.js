883386de9ef65439cb754066f0a89163056d72b6
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-09-29 22:11:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,16 @@
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
+  var request = new XMLHttpRequest();
   
-  
-  
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-};
+  request.onreadystatechange = function(){
+  if(request.readystate == XMLHttpRequest.DONE)
+  {
+      if(request.status == 200)
+      {
+          var counter = request.responseText;
+           counter = counter + 1;
+           var span = document.getElementById('count');
+      }
+  }
+  };
+ };
