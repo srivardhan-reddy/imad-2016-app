b996f730e837df1741ae6e816635e6458006ad14
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-10-24 21:25:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,18 +88,7 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool = new Pool(config);
-app.get('/test-db', function(req,res){
-    pool.query('SELECT * FROM test', function (err,result){
-       if (err)
-       { res.status(500).send(err.tostring());
-       }
-       else
-       {
-            res.send(JSON.stringify(result.rows));
-       }
-           
-    });
+
 });
 var counter = 0;
 app.get('/counter', function(req,res){
@@ -112,7 +101,18 @@ app.get('/submit-name', function(req,res){
    names.push(name);
    res.send(JSON.stringify(names));
 });
-
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    pool.query('SELECT * FROM test', function (err,result){
+       if (err)
+       { res.status(500).send(err.tostring());
+       }
+       else
+       {
+            res.send(JSON.stringify(result.rows));
+       }
+           
+    });
 app.get('/articles/:articleName', function (req,res){
 pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"' ",function (err,result){
    if(err)
