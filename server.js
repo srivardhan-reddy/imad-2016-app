efd2ba5c8fdc4ab70b5c8ab13870924102a57671
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-11-08 19:43:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/submit-name', function(req,res){
 var pool = new Pool(config);
 
 app.get('/articles/:articleName', function (req,res){
-pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"' ",function (err,result){
+pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'",function (err,result){
    if(err)
    {
        res.status(500).res.send(err.toString());
