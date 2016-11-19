79ea0dd28c0e1e8484861321640fb681fd95c743
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-11-19 12:52:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.post ('/login', function(req,res){
    var password = req.body.password;
    
    
-   pool.query('SELECT * from "user" username =$1',[username],function (err,result){
+   pool.query('SELECT * FROM "user" WHERE username =$1',[username],function (err,result){
        if (err)
        { res.status(500).send(err.tostring());
        }
