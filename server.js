c1ba56eaa431c78767bfa1bde8cc8a0bc9082e2d
srivardhan-reddy <vardhan998reddy@gmail.com>
2016-11-19 11:09:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ function hash (input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
-app.get('/hash/:inut', function(req,res){
+app.get('/hash/:input', function(req,res){
    
    var hashedString = hash (req.params.input,'random string');
    res.send(hashedString);
